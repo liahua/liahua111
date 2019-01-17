7839218583a6f89dcc3ea8f3391ae0464324b901
liahua <85178516@qq.com>
2019-01-17 16:47:55 +0800

    更新登出

--- jt-web/src/main/java/com/jt/idea/web/controller/UserController.java
+++ jt-web/src/main/java/com/jt/idea/web/controller/UserController.java
@@ -9,13 +9,17 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.JedisCluster;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private JedisCluster jedisCluster;
     @Autowired
     private TbUserService tbUserService;
 
@@ -60,9 +64,41 @@ public class UserController {
     }
 
     @RequestMapping("/logout")
-    public String logout(){
-        System.out.println("UserController.logout");
+    public String logout(HttpServletRequest req,HttpServletResponse resp){
+        /**
+         * 1.清除redis缓存
+         * 2.删除cookie(SetMaxAge)
+         * 3.跳转首页
+         */
+        String token=null;
+        Cookie[] cookies = req.getCookies();
+        for (Cookie cookie : cookies) {
+            String name = cookie.getName();
+            if(name.equals("JT_TICKET")){
+                token=cookie.getValue();
+                removeCookie(resp,name);
+                break;
+            }
+        }
+        try {
+            removeKeyFromRedis(token);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "redirect:/index.html";
     }
+
+    private void removeCookie(HttpServletResponse resp, String name) {
+        Cookie cookie = new Cookie(name, "");
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        resp.addCookie(cookie);
+    }
+
+    private void removeKeyFromRedis(String token) {
+        jedisCluster.del(token);
+    }
+
+
 
 }
